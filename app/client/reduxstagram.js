a5d81ec74d8a2fd1f72921854eb707855715f000
Sebastian Saier <sebastian.saier@web.de>
2016-07-16 02:54:45 +0200

    completed first part of tutorial

--- app/client/reduxstagram.js
+++ app/client/reduxstagram.js
@@ -2,7 +2,29 @@
 import React from 'react';
 import { render } from 'react-dom'; // import just the render method from react-dom
 
-// Import CSS
+// Import css
 import css from './styles/style.styl';
 
-render(<p>hi</p>, document.getElementById(root''));
+// Import Components
+import App from './components/App';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
+const router = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={PhotoGrid}></IndexRoute>
+        <Route path="/view/:postId" component={Single}></Route>
+      </Route>
+    </Router>
+  </Provider>
+)
+
+render(router, document.getElementById('root'));
+
